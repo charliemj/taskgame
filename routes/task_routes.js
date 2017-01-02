ea91ab4273f98b620993f97a47cf84c68f78b4f8
Karleigh Moore <karleighmoore19@gmail.com>
2017-01-02 14:25:56 -0500

    Added routing

--- routes/task_routes.js
+++ routes/task_routes.js
@@ -1,45 +1,46 @@
-// Reward routes
+// Task routes
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Reward = require('../models/rewards_model.js');
+var Task = require('../models/tasks_model.js');
 
-//get all rewards
+
+//get all tasks
 router.get("/",function(req,res){
-    // use mongoose to get all rewards in the database
+	// use mongoose to get all tasks in the database
     if(err){
         res.send(err);
     }//end if
     else{
-        Reward.find(function(err,rewards){
+        Task.find(function(err,tasks){
             if (err){
                 res.send(err);
             }//end if
             else{
-                res.json([rewards]);
+                res.json([tasks]);
             }//end else
         });//end find
     }//end else
 });
 
-//create a new reward and send it back to task list after creation
+//create a new task and send it back to task list after creation
 router.post('/',function(req,res){
-    // create a reward, information comes from AJAX request from Angular
-    Reward.create({
+	// create a task, information comes from AJAX request from Angular
+    Task.create({
         text: req.body.text,
-        cost: req.body.cost
-    }, function(err, reward){
+        points: req.body.points
+    }, function(err, task){
         if(err){
             res.send(err);
         }//end if
         else{
-        // get and return all the rewards after you create another
-            Reward.find(function(err, rewards){
+        // get and return all the tasks after you create another
+            Task.find(function(err, tasks){
                 if (err){
                     res.send(err);
                 }//end if
                 else{
-                    res.json([rewards]);
+                    res.json([tasks]);
                 }//end else
             });//end find
         }//end else
@@ -47,21 +48,21 @@ router.post('/',function(req,res){
 });
 
 //delete a task
-router.delete('/:reward_id',function(req,res){
-    Reward.remove({
-        _id : req.params.reward_id
-    }, function(err, reward){
+router.delete('/:task_id',function(req,res){
+	Task.remove({
+        _id : req.params.task_id
+    }, function(err, task){
         if (err){
             res.send(err);
         }//end if
         else{
-            // get and return all the rewards after you delete one
-            Reward.find(function(err, rewards){
+            // get and return all the tasks after you delete one
+            Task.find(function(err, tasks){
                 if(err){
                     res.send(err);
                 }//end if
                 else{
-                    res.json([rewards]);
+                    res.json([tasks]);
                 }//end else
             });//end find 
         }//end else
